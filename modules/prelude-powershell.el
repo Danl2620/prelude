75169c1fd251464891905c8f4f7f759436630190
Dan Liebgold <dan.liebgold@gmail.com>
2019-12-05 11:11:20 -0800

    missed file

---
+++ modules/prelude-powershell.el
@@ -0,0 +1,32 @@
+;;; prelude-powershell.el --- Emacs Prelude: PowerShell major mode for editing and running PowerShell files
+;;
+;;
+;; URL: https://github.com/bbatsov/prelude
+;; Version: 1.0.0
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(prelude-require-package 'powershell)
+
+(provide 'prelude-powershell)
+
+;;; prelude-powershell.el ends here
